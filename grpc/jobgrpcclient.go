c168bfd48191464dc73de679b592cff3c746c3ea
zyfdegh <zyfdegg@gmail.com>
2017-11-16 16:37:56 +0800

    add missing src/service/grpc/; rename a function
    Former-commit-id: bdea403f00f39468fe550327f7388f5aaa21bc8d

---
+++ grpc/jobgrpcclient.go
@@ -0,0 +1,64 @@
+package grpc
+
+import (
+	"context"
+
+	pb "bitbucket.org/linkernetworks/cv-tracker/src/grpc/messages"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+)
+
+// JobGrpcClient is a wrapper for Job gRPC client
+type JobGrpcClient struct {
+	serverAddr string // <host>:<port> of gRPC server
+	enableSSL  bool
+	conn       *grpc.ClientConn
+	grpcClient pb.JobClient // protobuf generated interface
+	// add cert when SSL is needed
+	sslCrt string // cert path
+}
+
+// NewInsecureJobClient creates a Job gPRC client (not secure)
+func NewInsecureJobClient(serverAddr string) (*JobGrpcClient, error) {
+	conn, err := grpc.Dial(serverAddr, grpc.WithInsecure())
+	if err != nil {
+		return nil, err
+	}
+
+	return &JobGrpcClient{
+		serverAddr: serverAddr,
+		enableSSL:  false,
+		conn:       conn,
+		grpcClient: pb.NewJobClient(conn),
+	}, nil
+}
+
+// NewSSLJobClient creates a Job gRPC client with SSL
+func NewSSLJobClient(serverAddr string, sslCrt string) (client *JobGrpcClient, err error) {
+	// gRPC SSL Reference: https://bbengfort.github.io/programmer/2017/03/03/secure-grpc.html
+	creds, err := credentials.NewClientTLSFromFile(sslCrt, "")
+	if err != nil {
+		return nil, err
+	}
+	conn, err := grpc.Dial(serverAddr, grpc.WithTransportCredentials(creds))
+	if err != nil {
+		return nil, err
+	}
+	return &JobGrpcClient{
+		serverAddr: serverAddr,
+		enableSSL:  true,
+		sslCrt:     sslCrt,
+		conn:       conn,
+		grpcClient: pb.NewJobClient(conn),
+	}, nil
+}
+
+// JobEnqueue call job enqueue
+func (c *JobGrpcClient) JobEnqueue(req *pb.JobRequest) (*pb.JobResponse, error) {
+	return c.grpcClient.JobEnqueue(context.Background(), req) // TODO context with
+}
+
+// Close connection
+func (c *JobGrpcClient) Close() error {
+	return c.conn.Close()
+}

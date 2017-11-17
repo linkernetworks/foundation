42ce0b965e01941d2d5224302239fbc4144e92c8
yeqing <yeqingsuc@gmail.com>
2017-11-17 18:17:05 +0800

    move initlogger to src
    Former-commit-id: 9ca8648e1ee1771ca12b5d59607b82ae8044c7ce

--- logger.go
+++ logger.go
@@ -1,6 +1,14 @@
 package logger
 
 import (
+	"log"
+	"os"
+	"path"
+	"time"
+
+	"bitbucket.org/linkernetworks/cv-tracker/server/config"
+	rotatelogs "github.com/lestrrat/go-file-rotatelogs"
+	"github.com/rifflock/lfshook"
 	"github.com/sirupsen/logrus"
 	prefixed "github.com/x-cray/logrus-prefixed-formatter"
 )
@@ -11,6 +19,69 @@ var Logger *logrus.Logger
 func init() {
 	Logger = logrus.New()
 	Logger.Formatter = new(prefixed.TextFormatter)
+}
+
+func New(c config.LoggerConfig) *logrus.Logger {
+	// preparing log dir
+	dir := c.Dir
+
+	if dir == "" {
+		dir = "log"
+		log.Println("Log dir is not specified. Using default log dir:", dir)
+	}
+
+	err := os.MkdirAll(dir, os.ModePerm)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	filePattern := c.FilePattern
+	if filePattern == "" {
+		filePattern = "access_log.%Y%m%d"
+	}
+
+	linkName := c.LinkName
+	if linkName == "" {
+		linkName = "access_log"
+	}
+
+	log.Println("Start writing log to", path.Join(dir, linkName))
+	writer, err := rotatelogs.New(
+		path.Join(dir, filePattern),
+		rotatelogs.WithLinkName(path.Join(dir, linkName)),
+		rotatelogs.WithMaxAge(24*time.Hour),
+		rotatelogs.WithRotationTime(time.Duration(24)*time.Hour),
+	)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	Logger.Hooks.Add(
+		lfshook.NewHook(
+			lfshook.WriterMap{
+				logrus.DebugLevel: writer,
+				logrus.InfoLevel:  writer,
+				logrus.WarnLevel:  writer,
+				logrus.ErrorLevel: writer,
+				logrus.FatalLevel: writer,
+			},
+		),
+	)
+
+	switch c.Level {
+	case "error":
+		Logger.SetLevel(logrus.ErrorLevel)
+	case "warn":
+		Logger.SetLevel(logrus.WarnLevel)
+	case "info":
+		Logger.SetLevel(logrus.InfoLevel)
+	case "debug":
+		Logger.SetLevel(logrus.DebugLevel)
+	default:
+		Logger.SetLevel(logrus.InfoLevel)
+	}
+
+	return Logger
 }
 
 func Info(args ...interface{}) {

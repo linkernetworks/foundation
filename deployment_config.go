d2f83d01198433bf3079a15a3c26b0d982f9dfb5
hwchiu <hwchiu@linkernetworks.com>
2017-12-16 22:19:54 +0800

    get the clietset from the KubeDeploymentConfig

--- deployment_config.go
+++ deployment_config.go
@@ -1,5 +1,10 @@
 package config
 
+import (
+	"bitbucket.org/linkernetworks/aurora/src/kubeconfig"
+	"k8s.io/client-go/kubernetes"
+)
+
 type DeploymentConfig struct {
 	Type       string               `json:"type"`
 	Kubernetes KubeDeploymentConfig `json:"kubernetes"`
@@ -9,4 +14,17 @@ type KubeDeploymentConfig struct {
 	ConfigFile string `json:"config"`
 	Context    string `json:"context"`
 	Namespace  string `json:"namespace"`
+}
+
+func (kcf KubeDeploymentConfig) CreateClientset() (*kubernetes.Clientset, error) {
+	config, err := kubeconfig.Load("", kcf.ConfigFile)
+	if err != nil {
+		return nil, err
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+	return clientset, nil
 }

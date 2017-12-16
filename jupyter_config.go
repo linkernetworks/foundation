60808bb54d43582ecb10d72a9a58e38ad8ca04da
David Chang <chechiachang999@gmail.com>
2017-12-16 21:55:19 +0800

    Refactor proxy handler. Adjust relative config

--- jupyter_config.go
+++ jupyter_config.go
@@ -1,8 +1,12 @@
 package config
 
 type JupyterConfig struct {
-	BaseUrl      string        `json:"baseUrl"`
-	LocalBaseUrl string        `json:"localBaseUrl"`
-	Localhost    string        `json:"localhost"`
-	Session      SessionConfig `json:"session:`
+	BaseUrl string        `json:"baseUrl"`
+	Session SessionConfig `json:"session"`
+	Dev     DevConfig     `json:"dev"`
+}
+
+type DevConfig struct {
+	BaseUrl string `json:"baseUrl"`
+	HostUrl string `json:"hostUrl"`
 }

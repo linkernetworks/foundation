0b29aa1d397b0f3291d4c5f562051ba238a3c3ad
c9s <yoanlin93@gmail.com>
2017-12-19 20:34:40 +0800

    refactor NotebookSpawner service

--- notebookspawner/service.go
+++ notebookspawner/service.go
@@ -8,8 +8,14 @@ import (
 	"bitbucket.org/linkernetworks/aurora/src/service/mongo"
 	"bitbucket.org/linkernetworks/aurora/src/service/notebookspawner/notebook"
 
+	v1 "k8s.io/api/core/v1"
+
 	"path/filepath"
 )
+
+type PodProvider interface {
+	NewPod(podName string) v1.Pod
+}
 
 type NotebookSpawnerService struct {
 	Config     config.Config
@@ -31,13 +37,15 @@ func (s *NotebookSpawnerService) Start(nb *entity.Notebook) error {
 	workspace := filepath.Join(s.Config.Data.BatchDir, "batch-"+nb.WorkspaceID.Hex())
 
 	// Start pod for notebook in workspace(batch)
-	nbs := internalservice.NewNotebookService(clientset, s.Mongo, notebook.KubeNotebook{
+	nbs := internalservice.NewNotebookService(clientset, s.Mongo)
+
+	knb := notebook.KubeNotebook{
 		Name:      nb.ID.Hex(),
 		Workspace: workspace,
 		ProxyURL:  s.Config.Jupyter.BaseUrl,
 		Image:     nb.Image,
-	})
-	if _, err := nbs.Start(); err != nil {
+	}
+	if _, err := nbs.Start(knb); err != nil {
 		return err
 	}
 	return nil
@@ -49,7 +57,7 @@ func (s *NotebookSpawnerService) Stop(nb *entity.Notebook) error {
 		return err
 	}
 
+	nbs := internalservice.NewNotebookService(clientset, s.Mongo)
 	knb := notebook.KubeNotebook{Name: nb.ID.Hex()}
-	nbs := internalservice.NewNotebookService(clientset, s.Mongo, knb)
-	return nbs.Stop()
+	return nbs.Stop(knb)
 }

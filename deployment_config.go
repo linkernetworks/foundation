6efd4320e595557dd80a3318b96a2208d4030d56
c9s <yoanlin93@gmail.com>
2017-12-19 17:09:10 +0800

    add kubernetes config to k8s.json

--- deployment_config.go
+++ deployment_config.go
@@ -1,30 +1,6 @@
 package config
 
-import (
-	"bitbucket.org/linkernetworks/aurora/src/kubeconfig"
-	"k8s.io/client-go/kubernetes"
-)
-
 type DeploymentConfig struct {
 	Type       string           `json:"type"`
 	Kubernetes KubernetesConfig `json:"kubernetes"`
-}
-
-type KubernetesConfig struct {
-	ConfigFile string `json:"config"`
-	Context    string `json:"context"`
-	Namespace  string `json:"namespace"`
-}
-
-func (kcf KubernetesConfig) CreateClientset() (*kubernetes.Clientset, error) {
-	config, err := kubeconfig.Load("", kcf.ConfigFile)
-	if err != nil {
-		return nil, err
-	}
-
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		return nil, err
-	}
-	return clientset, nil
 }

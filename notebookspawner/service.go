87c3c9c6d52197354512c5803dc613caf2d49523
c9s <yoanlin93@gmail.com>
2017-12-19 18:09:58 +0800

    Implement notebook spawner service

---
+++ notebookspawner/service.go
@@ -0,0 +1,47 @@
+package notebookspawner
+
+import (
+	"bitbucket.org/linkernetworks/aurora/src/config"
+	"bitbucket.org/linkernetworks/aurora/src/entity"
+	"bitbucket.org/linkernetworks/aurora/src/internalservice"
+	"bitbucket.org/linkernetworks/aurora/src/notebook"
+	"bitbucket.org/linkernetworks/aurora/src/service/kubernetes"
+	"bitbucket.org/linkernetworks/aurora/src/service/mongo"
+
+	"path/filepath"
+)
+
+type NotebookSpawnerService struct {
+	Config     config.Config
+	Mongo      *mongo.MongoService
+	Kubernetes *kubernetes.Service
+}
+
+func New(c config.Config, m *mongo.MongoService, k *kubernetes.Service) *NotebookSpawnerService {
+	return &NotebookSpawnerService{c, m, k}
+}
+
+func (s *NotebookSpawnerService) Start(nb *entity.Notebook) error {
+	clientset, err := s.Kubernetes.CreateClientset()
+	if err != nil {
+		return err
+	}
+
+	workspaceDir := filepath.Join(s.Config.Data.BatchDir, "batch-"+nb.WorkspaceID.Hex())
+
+	// Start pod for notebook in workspace(batch)
+	nbs := internalservice.NewNotebookService(clientset, s.Mongo, notebook.KubeNotebook{
+		Name:      nb.ID.Hex(),
+		WorkSpace: workspaceDir,
+		ProxyURL:  s.Config.Jupyter.BaseUrl,
+		Image:     nb.Image,
+	})
+	if _, err := nbs.Start(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *NotebookSpawnerService) Stop() {
+
+}

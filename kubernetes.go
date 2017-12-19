7e6b06b547d84a30500a379a0c58ed19f3763273
c9s <yoanlin93@gmail.com>
2017-12-19 17:23:51 +0800

    support KubernetesConfig.InCluster

--- kubernetes.go
+++ kubernetes.go
@@ -4,5 +4,5 @@ type KubernetesConfig struct {
 	ConfigFile string `json:"config"`
 	Context    string `json:"context"`
 	Namespace  string `json:"namespace"`
-	IsCluster  bool   `json:"inCluster"`
+	InCluster  bool   `json:"inCluster"`
 }

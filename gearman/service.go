9c902be81ed584429a200800261aa6733be44897
zyfdegh <zyfdegg@gmail.com>
2017-12-28 16:59:05 +0800

    replace log.Fatal with log.Printf (or error checking and return)

--- gearman/service.go
+++ gearman/service.go
@@ -2,7 +2,6 @@ package gearman
 
 import (
 	"bitbucket.org/linkernetworks/aurora/src/config"
-	"bitbucket.org/linkernetworks/aurora/src/logger"
 	"github.com/mikespook/gearman-go/client"
 )
 
@@ -15,15 +14,15 @@ func NewFromConfig(cf *config.GearmanConfig) *Service {
 	return &Service{Bind: addr}
 }
 
-func (g *Service) NewClient() *client.Client {
+func (g *Service) NewClient() (*client.Client, error) {
 	c, err := client.New(client.Network, g.Bind)
 	if err != nil {
-		logger.Fatal(err)
+		return nil, err
 	}
 	/*
 		c.ErrorHandler = func(e error) {
 			logger.Info("gearman client error:", e)
 		}
 	*/
-	return c
+	return c, nil
 }

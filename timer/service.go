3b908ab461577925fc4bb7c96b726ef362ea7a8b
c9s <yoanlin93@gmail.com>
2018-01-18 18:52:05 +0800

    move timer service to timer package

---
+++ timer/service.go
@@ -0,0 +1,41 @@
+package timer
+
+import (
+	"time"
+
+	"bitbucket.org/linkernetworks/aurora/src/logger"
+)
+
+type TimerHandler func()
+
+type TimerService struct {
+	Handlers map[string]TimerHandler
+}
+
+func New() *TimerService {
+	return &TimerService{}
+}
+
+func (s *TimerService) Bind(key string, handler TimerHandler) {
+	s.Handlers[key] = handler
+}
+
+func (s *TimerService) Run() {
+	ticker := time.NewTicker(3 * time.Minute)
+	quit := make(chan struct{})
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				for key, handler := range s.Handlers {
+					logger.Debugf("Running timer handler: %s", key)
+					go handler()
+				}
+
+			case <-quit:
+				ticker.Stop()
+				return
+			}
+		}
+	}()
+}

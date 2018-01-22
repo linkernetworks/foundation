f0779d514160894858af558abe202f74efd54bcb
zyfdegh <zyfdegg@gmail.com>
2018-01-22 15:10:28 +0800

    fix JSON indent

--- jobupdater_test.go
+++ jobupdater_test.go
@@ -10,14 +10,14 @@ import (
 func TestJobUpdaterConfig(t *testing.T) {
 	cf := JobUpdaterConfig{}
 	jsontext := `{
-		"bufferSize": 120,
-		"logger": {
+        "bufferSize": 120,
+        "logger": {
             "dir": "./logs",
             "filePattern": "jobupdater.log.%Y%m%d",
             "linkName": "jobupdater",
             "level": "debug"
         }
-	}`
+    }`
 	err := json.Unmarshal([]byte(jsontext), &cf)
 	assert.NoError(t, err)
 }

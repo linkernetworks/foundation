01fb164c884b1523fe5a219a61e69e9c440c1152
yeqing <yeqingsuc@gmail.com>
2018-01-31 17:21:42 +0800

    add creat volume restfull api

---
+++ nfs/service.go
@@ -0,0 +1,46 @@
+package nfs
+
+import (
+	"bitbucket.org/linkernetworks/aurora/src/service/kubernetes"
+	"bitbucket.org/linkernetworks/aurora/src/service/mongo"
+	v1 "k8s.io/api/core/v1"
+	kubernetesclient "k8s.io/client-go/kubernetes"
+)
+
+type NfsService struct {
+	Mongo      *mongo.Service
+	Context    *mongo.Session
+	Kubernetes *kubernetes.Service
+	clientset  *kubernetesclient.Clientset
+	namespace  string
+}
+
+func New(m *mongo.Service, k *kubernetes.Service) *NfsService {
+	return &NfsService{
+		Mongo:      m,
+		Context:    m.NewSession(),
+		Kubernetes: k,
+		namespace:  "default",
+	}
+}
+
+func (s *NfsService) getClientset() (*kubernetesclient.Clientset, error) {
+	if s.clientset != nil {
+		return s.clientset, nil
+	}
+	var err error
+	s.clientset, err = s.Kubernetes.CreateClientset()
+	return s.clientset, err
+}
+
+func (s *NfsService) CreatePV(pv *v1.PersistentVolume) error {
+	clientset, err := s.getClientset()
+	if err != nil {
+		return err
+	}
+	_, err = clientset.CoreV1().PersistentVolumes().Create(pv)
+	if err != nil {
+		return err
+	}
+	return nil
+}

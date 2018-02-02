245bbe6c6115ab83442435c0b10721a88f71744a
John-Lin <linton.tw@gmail.com>
2018-02-02 14:47:01 +0800

    add new service for messenger

--- messenger/service.go
+++ messenger/service.go
@@ -16,39 +16,55 @@ type MessageSender interface {
 }
 
 type Mailgun struct {
+	client mailgun.Mailgun
+
 	domain       string
-	ApiKey       string
+	apiKey       string
 	publicApiKey string
 }
 
 type Twilio struct {
+	client *twilio.TwilioClient
+
 	accountSid string
 	authToken  string
 }
 
-func (mg *Mailgun) NewService(m *mongo.Service) {
-	context := m.NewSession()
-	defer context.Close()
+func (mg *Mailgun) NewService(mongoService *mongo.Service) *Mailgun {
+	// context := mongoService.NewSession()
+	// defer context.Close()
 	// TODO Read config from mongo
 
-	mg.domain = "linkernetworks.com"
-	mg.ApiKey = "blahblah"
-	mg.publicApiKey = "blahblah"
+	domain := "sandbox86ffb85f5a8d44a6bf93f5bd29fcbb79.mailgun.org"
+	apiKey := "key-5edd1caa4140a3c11ee0cfd400c7c1b7"
+	publicApiKey := "pubkey-0c343ddc3036d36c8027cb56d0f9da7d"
+
+	return &Mailgun{
+		domain:       domain,
+		apiKey:       apiKey,
+		publicApiKey: publicApiKey,
+		client:       mailgun.NewMailgun(domain, apiKey, publicApiKey),
+	}
 }
 
-func (twlo *Twilio) NewService(m *mongo.Service) {
-	context := m.NewSession()
-	defer context.Close()
+func (twlo *Twilio) NewService(mongoService *mongo.Service) *Twilio {
+	// context := mongoService.NewSession()
+	// defer context.Close()
 	// TODO Read config from mongo
 
-	twlo.accountSid = "xxxxx"
-	twlo.authToken = "ddddd"
+	accountSid := "xxxxx"
+	authToken := "ddddd"
+
+	return &Twilio{
+		accountSid: accountSid,
+		authToken:  authToken,
+		client:     twilio.NewClient(accountSid, authToken),
+	}
 }
 
 func (mg *Mailgun) Send(msg Message) error {
-	client := mailgun.NewMailgun(mg.domain, mg.ApiKey, mg.publicApiKey)
-	message := client.NewMessage(msg.From(), msg.Title(), msg.Content(), msg.To())
-	resp, id, err := client.Send(message)
+	message := mg.client.NewMessage(msg.From(), msg.Title(), msg.Content(), msg.To())
+	resp, id, err := mg.client.Send(message)
 	if err != nil {
 		return err
 	}
@@ -57,8 +73,7 @@ func (mg *Mailgun) Send(msg Message) error {
 }
 
 func (twlo *Twilio) Send(msg Message) error {
-	client := twilio.NewClient(twlo.accountSid, twlo.authToken)
-	message, err := twilio.NewMessage(client, msg.Title(), msg.From(), twilio.Body(msg.Content()))
+	message, err := twilio.NewMessage(twlo.client, msg.Title(), msg.From(), twilio.Body(msg.Content()))
 	if err != nil {
 		return err
 	}

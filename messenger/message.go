6c4877a8277226356017fedbaeef89b036ffc564
John-Lin <linton.tw@gmail.com>
2018-02-05 18:47:48 +0800

    refactoring

--- messenger/message.go
+++ messenger/message.go
@@ -2,117 +2,43 @@ package messenger
 
 import (
 	"bitbucket.org/linkernetworks/aurora/src/entity"
-	"bitbucket.org/linkernetworks/aurora/src/logger"
-	"bitbucket.org/linkernetworks/aurora/src/service/mongo"
-	"gopkg.in/mgo.v2/bson"
+	_ "bitbucket.org/linkernetworks/aurora/src/logger"
+	_ "gopkg.in/mgo.v2/bson"
 )
 
-type Message interface {
-	GetTitle() string
-	GetContent() string
-	GetTo() bson.ObjectId
-	GetFrom() bson.ObjectId
-	GetSenderAddress() string
-	GetReceiverAddress() string
-	GetSenderPhoneNumber() string
-	GetReceiverPhoneNumber() string
+type MessageType int
+
+const (
+	EmailMessage MessageType = 1 << iota
+	SMSMessage
+)
+
+func NewMessage(t MessageType, title, content string, to, from *entity.User) entity.Messenger {
+	switch t {
+	case EmailMessage:
+		return newEmail(title, content, to, from)
+	case SMSMessage:
+		return newSMS(content, to, from)
+	default:
+		return nil
+	}
 }
 
-type Notification struct {
-	Content string
-	To      bson.ObjectId
-	From    bson.ObjectId
-}
-
-func (n *Notification) GetContent() string {
-	return n.Content
-}
-
-func (n *Notification) GetTo() bson.ObjectId {
-	return n.To
-}
-
-func (n *Notification) GetFrom() bson.ObjectId {
-	return n.From
-}
-
-type Email struct {
-	Notification
-	Title       string
-	ToAddress   string
-	FromAddress string
-}
-
-func NewEmail(title, content string, to, from *entity.User) *Email {
-	// fromAddress, _ := FindUserById(ms, from)
-	// sender := fromAddress.Email
+func newEmail(title, content string, to, from *entity.User) entity.Messenger {
 	sender := "noreply@linkernetworks.com"
-
-	// toAddress, _ := FindUserById(ms, to)
-	// receiver := toAddress.Email
-
-	return &Email{
-		Notification: Notification{
-			Content: content,
-			To:      to.ID,
-			From:    from.ID,
-		},
+	return &entity.Email{
 		Title:       title,
+		Content:     content,
 		ToAddress:   to.Email,
 		FromAddress: sender,
 	}
 }
 
-func (e *Email) GetTitle() string {
-	return e.Title
-}
-
-func (e *Email) GetSenderAddress() string {
-	return e.FromAddress
-}
-
-func (e *Email) GetReceiverAddress() string {
-	return e.ToAddress
-}
-
-type SMS struct {
-	Notification
-	ToNumber   string
-	FromNumber string
-}
-
-func NewSMS(content string, to, from *entity.User) *SMS {
-	// fromNumber, _ := FindUserById(ms, from)
-	// FIXME the trial account can not use custom phone number
+func newSMS(content string, to, from *entity.User) entity.Messenger {
 	sender := "+19284409015"
-
-	return &SMS{
-		Notification: Notification{
-			Content: content,
-			To:      to.ID,
-			From:    from.ID,
-		},
+	return &entity.SMS{
+		Content:    content,
 		ToNumber:   to.Cellphone,
 		FromNumber: sender,
 	}
-}
-
-func (s *SMS) GetSenderPhoneNumber() string {
-	return s.FromNumber
-}
-
-func (s *SMS) GetReceiverPhoneNumber() string {
-	return s.ToNumber
-}
-
-func FindUserById(ms *mongo.Service, uid bson.ObjectId) (*entity.User, error) {
-	context := ms.NewSession()
-	defer context.Close()
-	user := &entity.User{}
-	err := context.C(entity.UserCollectionName).FindId(uid).One(user)
-	if err != nil {
-		logger.Errorf("Find user fail: %s", err.Error())
-		return nil, err
-	}
-	return user, nil
 }
--- messenger/message_test.go
+++ messenger/message_test.go
@@ -1,52 +1,13 @@
 package messenger
 
 import (
-	"bitbucket.org/linkernetworks/aurora/src/config"
 	"bitbucket.org/linkernetworks/aurora/src/entity"
-	"bitbucket.org/linkernetworks/aurora/src/service/mongo"
 	"gopkg.in/mgo.v2/bson"
 
 	"github.com/stretchr/testify/assert"
-	_ "log"
+	// "log"
 	"testing"
 )
-
-func TestFindUserById(t *testing.T) {
-	const testingConfigPath = "../../../config/testing.json"
-	var err error
-	cf := config.MustRead(testingConfigPath)
-
-	ms := mongo.New(cf.Mongo.Url)
-	assert.NotNil(t, ms)
-
-	context := ms.NewSession()
-	defer context.Close()
-
-	newUser := &entity.User{
-		ID:        bson.ObjectId("123456789012"),
-		Email:     "develop@linkernetworks.com",
-		FirstName: "john",
-		LastName:  "lin",
-		Cellphone: "0987654556",
-		Roles:     nil,
-		Verified:  false,
-		Revoked:   false,
-	}
-
-	err = context.C(entity.UserCollectionName).Insert(newUser)
-	assert.NoError(t, err)
-
-	result := entity.User{}
-	err = context.C(entity.UserCollectionName).Find(bson.M{"first_name": "john"}).One(&result)
-	assert.NoError(t, err)
-
-	found, err := FindUserById(ms, result.ID)
-	assert.NoError(t, err)
-	assert.Equal(t, "develop@linkernetworks.com", found.Email)
-
-	err = context.DropCollection(entity.UserCollectionName)
-	assert.NoError(t, err)
-}
 
 func TestNewEmail(t *testing.T) {
 	toUser := &entity.User{
@@ -74,12 +35,14 @@ func TestNewEmail(t *testing.T) {
 	title := "Hello world"
 	content := "This is a long content. This is a long content. This is a long content. This is a long content."
 
-	e := NewEmail(title, content, toUser, fromUser)
+	e := NewMessage(EmailMessage, title, content, toUser, fromUser)
+	// e := NewEmail(title, content, toUser, fromUser)
 	assert.NotNil(t, e)
 	assert.Equal(t, "Hello world", e.GetTitle())
+	// log.Printf("%+v", e)
 	assert.Equal(t, "This is a long content. This is a long content. This is a long content. This is a long content.", e.GetContent())
-	assert.Equal(t, "develop@linkernetworks.com", e.GetReceiverAddress())
-	assert.Equal(t, "noreply@linkernetworks.com", e.GetSenderAddress())
+	assert.Equal(t, "develop@linkernetworks.com", e.GetTo())
+	assert.Equal(t, "noreply@linkernetworks.com", e.GetFrom())
 }
 
 func TestNewSMS(t *testing.T) {
@@ -107,9 +70,10 @@ func TestNewSMS(t *testing.T) {
 
 	content := "This is a long content. This is a long content. This is a long content. This is a long content."
 
-	sms := NewSMS(content, toUser, fromUser)
+	sms := NewMessage(SMSMessage, "", content, toUser, fromUser)
+	// sms := NewSMS(content, toUser, fromUser)
 	assert.NotNil(t, sms)
 	assert.Equal(t, "This is a long content. This is a long content. This is a long content. This is a long content.", sms.GetContent())
-	assert.Equal(t, "+886952301269", sms.GetReceiverPhoneNumber())
-	assert.Equal(t, "+19284409015", sms.GetSenderPhoneNumber())
+	assert.Equal(t, "+886952301269", sms.GetTo())
+	assert.Equal(t, "+19284409015", sms.GetFrom())
 }

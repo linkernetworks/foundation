30cd35defd9916f1638e54200d84e8d8d849b95f
c9s <yoanlin93@gmail.com>
2018-02-10 19:26:51 +0800

    Add Max function for comparing Duration

--- timeutils/time.go
+++ timeutils/time.go
@@ -9,6 +9,13 @@ func Now() *time.Time {
 	return &t
 }
 
+func Max(d1, d2 time.Duration) time.Duration {
+	if d1 < d2 {
+		return d2
+	}
+	return d1
+}
+
 func TruncateRedisDateTime(t *time.Time, d time.Duration) *time.Time {
 	if t != nil {
 		result := t.Truncate(d)

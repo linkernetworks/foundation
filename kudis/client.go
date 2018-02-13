153d382ddf391e2e9a8755903d3c6ed7e6fed16c
c9s <yoanlin93@gmail.com>
2018-02-13 13:37:13 +0800

    implement triggers to subscribe pod logs and pod events

--- kudis/client.go
+++ kudis/client.go
@@ -1,18 +1,16 @@
 package kudis
 
 import (
-	"context"
-
 	pb "bitbucket.org/linkernetworks/aurora/src/kubernetes/kudis/pb"
 	"google.golang.org/grpc"
 )
 
 // Client is a wrapper for container log piper gRPC client
 type Client struct {
-	serverAddr string // <host>:<port> of gRPC server
-	enableSSL  bool
-	conn       *grpc.ClientConn
-	client     pb.SubscriptionServiceClient // protobuf generated interface
+	pb.SubscriptionServiceClient        // protobuf generated interface
+	serverAddr                   string // <host>:<port> of gRPC server
+	enableSSL                    bool
+	conn                         *grpc.ClientConn
 }
 
 // NewInsecure creates a Job gPRC client (not secure)
@@ -23,15 +21,11 @@ func NewInsecure(serverAddr string) (*Client, error) {
 	}
 
 	return &Client{
-		serverAddr: serverAddr,
-		enableSSL:  false,
-		conn:       conn,
-		client:     pb.NewSubscriptionServiceClient(conn),
+		SubscriptionServiceClient: pb.NewSubscriptionServiceClient(conn),
+		serverAddr:                serverAddr,
+		enableSSL:                 false,
+		conn:                      conn,
 	}, nil
-}
-
-func (c *Client) SubscribePodLogs(req *pb.PodLogSubscriptionRequest) (*pb.SubscriptionResponse, error) {
-	return c.client.SubscribePodLogs(context.Background(), req)
 }
 
 // Close connection

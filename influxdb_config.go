82205bea8dd2d88a05ffa6e22f3c5479cd3166c8
yunchen <yunchen@nisra.net>
2018-02-15 12:35:52 +0800

    add import vender binary

--- influxdb_config.go
+++ influxdb_config.go
@@ -1,5 +1,7 @@
 package config
 
 type InfluxdbConfig struct {
-	Url string `json:"url" bson:"url"`
+	Url      string `json:"url" bson:"url"`
+	Database string `json:"database" bson:"database"`
+	Series   string `json:"series" bson:"series"`
 }

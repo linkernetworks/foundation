480c6d7416b6d99d0a3ce4588f501aa40858643f
c9s <yoanlin93@gmail.com>
2018-03-19 11:46:59 +0800

    Fix return parameters

--- notebookspawner/service.go
+++ notebookspawner/service.go
@@ -109,7 +109,7 @@ func (s *NotebookSpawnerService) Start(nb *entity.Notebook) (tracker *podtracker
 	if workspace.PrimaryVolume == nil {
 		vm := volumemanager.New(s.clientset, s.Session, "default")
 		if err := vm.CreatePrimaryVolume(&workspace); err != nil {
-			return fmt.Errorf("failed to prepare primary volume: %v", err)
+			return nil, fmt.Errorf("failed to prepare primary volume: %v", err)
 		}
 	}
 

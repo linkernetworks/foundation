f354d7dca7c066bc0b35d1743c26d73e8fc6e853
c9s <yoanlin93@gmail.com>
2018-03-20 12:24:05 +0800

    service/kubernetes: show the context that will be used

--- kubernetes/service.go
+++ kubernetes/service.go
@@ -3,6 +3,7 @@ package kubernetes
 import (
 	"bitbucket.org/linkernetworks/aurora/src/config"
 	"bitbucket.org/linkernetworks/aurora/src/kubeconfig"
+	"bitbucket.org/linkernetworks/aurora/src/logger"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 
@@ -27,6 +28,7 @@ func (s *Service) LoadConfig() (*rest.Config, error) {
 		return config, nil
 	}
 
+	logger.Debugf("Loading kubernetes config with context=%s from config=%s", s.Config.Context, s.Config.ConfigFile)
 	config, err := kubeconfig.Load(s.Config.Context, s.Config.ConfigFile)
 	if err != nil {
 		return nil, err

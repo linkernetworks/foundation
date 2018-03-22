a906bec277f521e249f59dd1f76353dfef2f772f
c9s <yoanlin93@gmail.com>
2018-03-22 18:45:07 +0800

    appspawner: implement appspawner

---
+++ appspawner/pod_factory.go
@@ -0,0 +1,113 @@
+package appspawner
+
+import (
+	"strconv"
+
+	"bitbucket.org/linkernetworks/aurora/src/entity"
+
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"k8s.io/apimachinery/pkg/api/resource"
+)
+
+// The container port of jupyter notebook
+const DefaultNotebookContainerPort = 8888
+
+type NotebookPodParameters struct {
+	WorkDir string
+	Bind    string
+	Port    int32
+}
+
+// NotebookPodFactory handle the process of creating the jupyter notebook pod
+type NotebookPodFactory struct {
+	params NotebookPodParameters
+}
+
+func NewNotebookPodFactory(params NotebookPodParameters) *NotebookPodFactory {
+	return &NotebookPodFactory{params}
+}
+
+// NewPod returns the Pod object of the jupyternotebook
+func (nb *NotebookPodFactory) NewPod(notebook *entity.Notebook) v1.Pod {
+	podName := notebook.DeploymentID()
+	volumes := []v1.Volume{
+		{
+			Name: "config-volume",
+			VolumeSource: v1.VolumeSource{
+				ConfigMap: &v1.ConfigMapVolumeSource{
+					LocalObjectReference: v1.LocalObjectReference{
+						Name: "jupyter-notebook-config",
+					},
+				},
+			},
+		},
+	}
+
+	mounts := []v1.VolumeMount{
+		{
+			Name:      "config-volume",
+			MountPath: "/home/jovyan/.jupyter/custom",
+		},
+	}
+
+	return v1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: podName,
+			Labels: map[string]string{
+				"service":   "notebook",
+				"workspace": notebook.WorkspaceID.Hex(),
+				"user":      notebook.CreatedBy.Hex(),
+			},
+		},
+		Spec: v1.PodSpec{
+			RestartPolicy: v1.RestartPolicyNever,
+			Containers: []v1.Container{
+				{
+					Name:            "notebook",
+					Image:           notebook.Image,
+					ImagePullPolicy: v1.PullIfNotPresent,
+					Args: []string{
+						"start-notebook.sh",
+						"--notebook-dir=" + nb.params.WorkDir,
+						"--ip=" + nb.params.Bind,
+						"--port=" + strconv.Itoa(int(nb.params.Port)),
+						"--NotebookApp.base_url=" + notebook.Url,
+						"--NotebookApp.token=''",
+						"--NotebookApp.allow_origin='*'",
+						"--NotebookApp.disable_check_xsrf=True",
+						"--Session.debug=True",
+					},
+					//FIXME we should also mount the PrimaryVolume.
+					VolumeMounts: mounts,
+					Ports: []v1.ContainerPort{
+						{
+							Name:          "notebook",
+							ContainerPort: nb.params.Port,
+							Protocol:      v1.ProtocolTCP,
+						},
+					},
+					Env: []v1.EnvVar{
+						{
+							Name:  "CPU_GUARANTEE",
+							Value: "50m",
+						},
+						{
+							Name:  "MEM_GUARANTEE",
+							Value: "64Mi",
+						},
+					},
+					Resources: v1.ResourceRequirements{
+						Limits: v1.ResourceList{"cpu": resource.MustParse("1000m")},
+						Requests: v1.ResourceList{
+							"cpu":    resource.MustParse("50m"),
+							"memory": resource.MustParse("64Mi"),
+						},
+					},
+				},
+			},
+			Volumes: volumes,
+		},
+	}
+}
---
+++ appspawner/service.go
@@ -0,0 +1,143 @@
+package appspawner
+
+import (
+	"errors"
+
+	// "bitbucket.org/linkernetworks/aurora/src/aurora/provision/path"
+	"bitbucket.org/linkernetworks/aurora/src/config"
+	"bitbucket.org/linkernetworks/aurora/src/entity"
+	"bitbucket.org/linkernetworks/aurora/src/kubernetes/pod/podproxy"
+	"bitbucket.org/linkernetworks/aurora/src/kubernetes/pod/podtracker"
+	"bitbucket.org/linkernetworks/aurora/src/kubernetes/types"
+	// "bitbucket.org/linkernetworks/aurora/src/types/container"
+
+	"bitbucket.org/linkernetworks/aurora/src/service/mongo"
+	"bitbucket.org/linkernetworks/aurora/src/service/redis"
+	"bitbucket.org/linkernetworks/aurora/src/workspace"
+
+	"k8s.io/client-go/kubernetes"
+
+	kerrors "k8s.io/apimachinery/pkg/api/errors"
+
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+var ErrAlreadyStopped = errors.New("Notebook is already stopped")
+
+type AppSpawnerService struct {
+	Config  config.Config
+	Mongo   *mongo.Service
+	Factory *NotebookPodFactory
+
+	Updater *podproxy.DocumentProxyInfoUpdater
+
+	clientset *kubernetes.Clientset
+	namespace string
+}
+
+func New(c config.Config, service *mongo.Service, clientset *kubernetes.Clientset, rds *redis.Service) *AppSpawnerService {
+	return &AppSpawnerService{
+		Factory: NewNotebookPodFactory(NotebookPodParameters{
+			WorkDir: c.Jupyter.WorkingDir,
+			Bind:    c.Jupyter.Address,
+			Port:    DefaultNotebookContainerPort,
+		}),
+		Config:    c,
+		Mongo:     service,
+		namespace: "default",
+		clientset: clientset,
+		Updater: &podproxy.DocumentProxyInfoUpdater{
+			Clientset: clientset,
+			Namespace: "default",
+			Redis:     rds,
+			PortName:  "notebook",
+		},
+	}
+}
+
+func (s *AppSpawnerService) NewPod(nb *entity.Notebook) (v1.Pod, error) {
+	session := s.Mongo.NewSession()
+	defer session.Close()
+
+	pod := s.Factory.NewPod(nb)
+
+	// load the workspace from the mongodb
+	ws, err := workspace.Load(session, nb.WorkspaceID)
+	if err != nil {
+		return pod, err
+	}
+
+	// attach the primary volumes to the pod spec
+	if err := workspace.AttachVolumesToPod(ws, &pod); err != nil {
+		return pod, err
+	}
+
+	return pod, nil
+}
+
+func (s *AppSpawnerService) Start(ws *entity.Workspace, nb *entity.Notebook) (tracker *podtracker.PodTracker, err error) {
+	pod, err := s.NewPod(nb)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// Start tracking first
+	_, err = s.getPod(nb)
+	if kerrors.IsNotFound(err) {
+		// Pod not found. Start a pod for notebook in workspace(batch)
+		tracker, err = s.Updater.TrackAndSyncUpdate(nb)
+		if err != nil {
+			return nil, err
+		}
+
+		_, err = s.clientset.CoreV1().Pods(s.namespace).Create(&pod)
+		if err != nil {
+			tracker.Stop()
+			return nil, err
+		}
+		return tracker, nil
+
+	} else if err != nil {
+		// unknown error
+		return nil, err
+	}
+
+	tracker, err = s.Updater.TrackAndSyncUpdate(nb)
+	return tracker, err
+}
+
+func (s *AppSpawnerService) getPod(doc types.DeploymentIDProvider) (*v1.Pod, error) {
+	return s.clientset.CoreV1().Pods(s.namespace).Get(doc.DeploymentID(), metav1.GetOptions{})
+}
+
+// Stop returns nil if it's already stopped
+func (s *AppSpawnerService) Stop(ws *entity.Workspace, notebook *entity.Notebook) (*podtracker.PodTracker, error) {
+	// if it's not created
+	_, err := s.getPod(notebook)
+	if kerrors.IsNotFound(err) {
+		return nil, ErrAlreadyStopped
+	} else if err != nil {
+		return nil, err
+	}
+
+	s.Updater.Reset(notebook)
+
+	// We found the pod, let's start a tracker first, and then delete the pod
+	tracker, err := s.Updater.TrackAndSyncDelete(notebook)
+	if err != nil {
+		return nil, err
+	}
+
+	podName := notebook.DeploymentID()
+	err = s.clientset.CoreV1().Pods(s.namespace).Delete(podName, &metav1.DeleteOptions{})
+	if err != nil {
+		defer tracker.Stop()
+		if kerrors.IsNotFound(err) {
+			return nil, ErrAlreadyStopped
+		}
+		return nil, err
+	}
+	return tracker, nil
+}

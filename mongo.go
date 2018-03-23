ab6dd441ce700143292a99b658998ac7b1b0e5c6
John-Lin <linton.tw@gmail.com>
2018-03-23 23:17:27 +0800

    log mongo panic

--- mongo.go
+++ mongo.go
@@ -2,6 +2,7 @@ package mongo
 
 import (
 	"bitbucket.org/linkernetworks/aurora/src/config"
+	"log"
 
 	"gopkg.in/mgo.v2"
 )
@@ -14,6 +15,7 @@ type Service struct {
 func New(url string) *Service {
 	session, err := mgo.Dial(url)
 	if err != nil {
+		log.Printf("Mongo Panic %v", err)
 		panic(err)
 	}
 	return &Service{

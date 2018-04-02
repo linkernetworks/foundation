18965cca0c6cd8496efcd642e5f3af415c0b72f2
c9s <yoanlin93@gmail.com>
2018-04-02 18:22:16 +0800

    Call kudis.NewClient to connect to kudis on request

--- kudis/service.go
+++ kudis/service.go
@@ -1,5 +1,17 @@
 package kudis
 
+import (
+	"bitbucket.org/linkernetworks/aurora/src/config"
+)
+
 type Service struct {
-	Client *Client
+	Config *config.KudisConfig
+}
+
+func New(cf *config.KudisConfig) *Service {
+	return &Service{cf}
+}
+
+func (s *Service) NewClient() (*Client, error) {
+	return NewInsecure(s.Config.Addr())
 }

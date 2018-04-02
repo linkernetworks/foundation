b90882c11ebe4c4e5be1cb610852bf9ef50ce3cc
c9s <yoanlin93@gmail.com>
2018-04-02 17:59:46 +0800

    remove address and working dir from jupyter notebook config

--- jupyter.go
+++ jupyter.go
@@ -3,14 +3,8 @@ package config
 type JupyterConfig struct {
 	BaseURL string `json:"baseUrl"`
 
-	// the address that the jupyter notebook will bind to "0.0.0.0"
-	Address string `json:"bind"`
-
 	// the default jupyternotebook docker image name
 	DefaultImage string `json:"defaultImage"`
-
-	// the working dir of the jupyter notebook process
-	WorkingDir string `json:"workingDir"`
 
 	// the cache configuration
 	Cache *JupyterCacheConfig `json:"cache"`

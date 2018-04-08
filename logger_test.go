3dc16b8f40f01f87a897eabd785a7965b921bef3
zyfdegh <zyfdegg@gmail.com>
2018-04-08 12:14:04 +0800

    make MaxAge configurable in LoggerConfig

---
+++ logger_test.go
@@ -0,0 +1,27 @@
+package config
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLoggerConfig(t *testing.T) {
+	cf := LoggerConfig{}
+	jsontext := `{
+            "dir": "./logs",
+            "filePattern": "migration.log.%Y%m%d",
+            "linkName": "migration",
+            "level": "debug",
+            "reserve": 30
+        }`
+	err := json.Unmarshal([]byte(jsontext), &cf)
+	assert.NoError(t, err)
+
+	assert.Equal(t, cf.Dir, "./logs")
+	assert.Equal(t, cf.FilePattern, "migration.log.%Y%m%d")
+	assert.Equal(t, cf.LinkName, "migration")
+	assert.Equal(t, cf.Level, "debug")
+	assert.Equal(t, cf.Reserve, 30)
+}

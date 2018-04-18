6688e2380443c69466f7e9e6f1aad273746a97e0
yoyopan <span@linkernetworks.com>
2018-04-18 12:09:49 +0800

    extract service provider

---
+++ provider/provider.go
@@ -0,0 +1,193 @@
+package provider
+
+import (
+	"time"
+
+	"bitbucket.org/linkernetworks/aurora/src/config"
+	"bitbucket.org/linkernetworks/aurora/src/logger"
+
+	// "bitbucket.org/linkernetworks/aurora/src/kudisclient"
+	"bitbucket.org/linkernetworks/aurora/src/jobclient"
+	"bitbucket.org/linkernetworks/aurora/src/service/appspawner"
+	"bitbucket.org/linkernetworks/aurora/src/service/gearman"
+	"bitbucket.org/linkernetworks/aurora/src/service/googlemap"
+	hdfsservice "bitbucket.org/linkernetworks/aurora/src/service/hdfs"
+	"bitbucket.org/linkernetworks/aurora/src/service/influxdb"
+	"bitbucket.org/linkernetworks/aurora/src/service/kubernetes"
+	"bitbucket.org/linkernetworks/aurora/src/service/kudis"
+	"bitbucket.org/linkernetworks/aurora/src/service/mongo"
+	"bitbucket.org/linkernetworks/aurora/src/service/redis"
+	"bitbucket.org/linkernetworks/aurora/src/service/session"
+	"bitbucket.org/linkernetworks/aurora/src/service/socketio"
+	"bitbucket.org/linkernetworks/aurora/src/service/timer"
+	"bitbucket.org/linkernetworks/aurora/src/service/websocket"
+	"bitbucket.org/linkernetworks/aurora/src/service/workspacefsspawner"
+	"github.com/colinmarc/hdfs"
+)
+
+type Services struct {
+	Config    config.Config
+	Redis     *redis.Service
+	Socketio  *socketio.Service
+	Timer     *timer.TimerService
+	Mongo     *mongo.Service
+	Gearman   *gearman.Service
+	WebSocket *websocket.WebSocketService
+	JobClient *jobclient.Service
+	Kudis     *kudis.Service
+	Influxdb  *influxdb.InfluxdbService
+	GoogleMap *googlemap.GoogleMapService
+	HDFS      *hdfs.Client
+
+	// Kubernetes service for notebooks
+	Kubernetes *kubernetes.Service
+
+	AppSpawner *appspawner.AppSpawner
+
+	// The Spawner for fileserver, each workspace has its own fileserver to mount the volume
+	WorkspaceSpawner *workspacefsspawner.WorkspaceFileServerSpawner
+}
+
+type ServiceDiscoverResponse struct {
+	Services map[string]Service `json:"services"`
+}
+
+type Service interface{}
+
+type SmartTrackerService struct {
+	Redis     config.ServiceConfig `json:"redis"`
+	Gearman   config.ServiceConfig `json:"gearman"`
+	Memcached config.ServiceConfig `json:"memcached"`
+}
+
+func NewRedisService(cf *config.RedisConfig) *redis.Service {
+	logger.Infof("Connecting to redis: %s", cf.Addr())
+	return redis.New(cf)
+}
+
+func NewInfluxdbService(cf *config.InfluxdbConfig) *influxdb.InfluxdbService {
+	logger.Infof("Connecting to influxdb: %s", cf.Url)
+	return &influxdb.InfluxdbService{Url: cf.Url}
+}
+
+func NewServiceProviderFromConfig(cf config.Config) *Services {
+	// setup logger configuration
+	logger.Setup(cf.App.Logger)
+
+	redisService := NewRedisService(cf.Redis)
+	socketService := socketio.New(cf.Socketio)
+
+	gcService := timer.New(30 * time.Minute)
+	gcService.Bind("cv-client", func() error {
+		return redisService.RemoveExpiredClients()
+	})
+	gcService.Bind("client-subscription", func() error {
+		return socketService.CleanUp()
+	})
+	gcService.Run()
+
+	err := newSessionService(cf.App.Session, cf.Redis.Addr())
+	for err != nil {
+		logger.Errorf("session initialization failed, retrying: %v", err)
+		err = newSessionService(cf.App.Session, cf.Redis.Addr())
+		time.Sleep(time.Second * 1)
+	}
+
+	logger.Infof("Connecting to jobserver via gRPC: %s", cf.JobController.Addr())
+	jobClient, err := jobclient.NewInsecure(cf.JobController.Addr())
+	if err != nil {
+		panic(err)
+	}
+
+	logger.Infof("Using kudis via gRPC: %s", cf.Kudis.Addr())
+
+	logger.Infof("Connecting to mongodb: %s", cf.Mongo.Url)
+	mongo := mongo.New(cf.Mongo.Url)
+
+	sp := &Services{
+		Config:    cf,
+		Redis:     redisService,
+		Socketio:  socketService,
+		Timer:     gcService,
+		Mongo:     mongo,
+		WebSocket: websocket.NewWebSocketService(),
+		JobClient: &jobclient.Service{Client: jobClient},
+		Influxdb:  NewInfluxdbService(cf.Influxdb),
+		GoogleMap: googlemap.New(cf.GoogleMap.Key),
+	}
+
+	if cf.Gearman != nil {
+		sp.Gearman = gearman.NewFromConfig(cf.Gearman)
+	} else {
+		logger.Warnln("Gearman service is not loaded: gearman config is not defined.")
+	}
+
+	// services that depends on Kubernetes
+	if cf.Kubernetes == nil {
+		logger.Warnln("kubernetes service is not loaded: kubernetes config is not defined.")
+	} else {
+		if cf.Kudis == nil {
+			logger.Warnln("kudis service is not loaded: kudis config is not defined.")
+		} else {
+			sp.Kudis = kudis.New(cf.Kudis)
+		}
+
+		sp.Kubernetes = kubernetes.NewFromConfig(cf.Kubernetes)
+		clientset, err := sp.Kubernetes.NewClientset()
+		if err == nil {
+			if cf.Jupyter == nil {
+				logger.Warnln("jupyter noteook spawner service is not loaded: jupyter notebook config is not defined.")
+			} else {
+				sp.AppSpawner = appspawner.New(cf, clientset, sp.Redis, sp.Mongo)
+			}
+			sp.WorkspaceSpawner = workspacefsspawner.New(cf, sp.Mongo, clientset, sp.Redis)
+		} else {
+			logger.Errorf("failed to create clientset, worksapce file server/notebook is not enabled: error=%v", err)
+		}
+	}
+
+	// optional services
+	if cf.Hdfs != nil && cf.Hdfs.Host != "" && cf.Hdfs.Enabled {
+		sp.HDFS = NewHdfsService(cf)
+	}
+	return sp
+}
+
+func NewHdfsService(cf config.Config) *hdfs.Client {
+	url := cf.Hdfs.Addr()
+	logger.Info("Connecting to HDFS: ", url, cf.Hdfs.Username)
+	client, err := hdfsservice.NewClientForUser(url, cf.Hdfs.Username)
+
+	if err != nil {
+		logger.Errorln("Can't connect to HDFS: ", url, cf.Hdfs.Username)
+	}
+
+	return client
+
+}
+
+func NewServiceProvider(configPath string) *Services {
+	cf := config.MustRead(configPath)
+	return NewServiceProviderFromConfig(cf)
+}
+
+func (s *Services) DiscoverServices() map[string]Service {
+	return map[string]Service{
+		"smartTrackerService": SmartTrackerService{
+			Redis:     s.Config.Redis.GetPublic(),
+			Gearman:   s.Config.Gearman.GetPublic(),
+			Memcached: s.Config.Memcached.GetPublic(),
+		},
+	}
+}
+
+func newSessionService(sc *config.SessionConfig, redisAddr string) error {
+	return session.NewService(
+		sc.Size,
+		sc.Protocal,
+		redisAddr,
+		sc.Password,
+		sc.Age,
+		sc.KeyPair,
+	)
+}

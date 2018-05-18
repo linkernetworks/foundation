5cfb1666fdd2dbe4a93c416df0eaaf589c2f476e
David Chang <chechiachang999@gmail.com>
2018-05-18 15:52:33 +0800

    Move config.MongoConfig to src/service/mongo

--- mongo_test.go
+++ mongo_test.go
@@ -5,18 +5,20 @@ import (
 	"testing"
 
 	"gopkg.in/mgo.v2/bson"
-
-	"bitbucket.org/linkernetworks/aurora/src/config"
 )
 
 func TestNewFromConfig(t *testing.T) {
-	cf := config.MustRead("../../../config/testing.json")
+	cf := MongoConfig{
+		Url: "mongodb://localhost:27017/aurora",
+	}
 	m := NewFromConfig(cf.Mongo)
 	assert.NotNil(t, m)
 }
 
 func TestNewSession(t *testing.T) {
-	cf := config.MustRead("../../../config/testing.json")
+	cf := MongoConfig{
+		Url: "mongodb://localhost:27017/aurora",
+	}
 	m := NewFromConfig(cf.Mongo)
 	assert.NotNil(t, m)
 
@@ -25,7 +27,9 @@ func TestNewSession(t *testing.T) {
 }
 
 func TestSessionFind(t *testing.T) {
-	cf := config.MustRead("../../../config/testing.json")
+	cf := MongoConfig{
+		Url: "mongodb://localhost:27017/aurora",
+	}
 	m := NewFromConfig(cf.Mongo)
 	s := m.NewSession()
 	var records []map[string]interface{}

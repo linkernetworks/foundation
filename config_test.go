873df4ff3dd363dfec84190f3485a8265b43aa9e
David Chang <chechiachang999@gmail.com>
2018-05-25 14:40:19 +0800

    Add example json

--- config_test.go
+++ config_test.go
@@ -7,38 +7,14 @@ import (
 )
 
 func TestReadTestingConfig(t *testing.T) {
-	configPath := "../../config/testing.json"
+	configPath := "example.json"
 	cf := MustRead(configPath)
 	assert.Equal(t, "localhost", cf.Redis.Host)
 	assert.NotEqual(t, int32(0), cf.Redis.Port)
-}
-
-func TestReadK8SConfig(t *testing.T) {
-	configPath := "../../config/k8s.json"
-	cf := MustRead(configPath)
-	assert.Equal(t, "redis.default", cf.Redis.Host)
-	assert.NotEqual(t, int32(0), cf.Redis.Port)
-}
-
-func TestReadLocalConfig(t *testing.T) {
-	configPath := "../../config/local.json"
-	cf := MustRead(configPath)
-	assert.Equal(t, "localhost", cf.Redis.Host)
-	assert.NotEqual(t, int32(0), cf.Redis.Port)
-}
-
-func TestReadTestingHdfsConfig(t *testing.T) {
-	configPath := "../../config/testing.json"
-	cf := MustRead(configPath)
-	if cf.Hdfs != nil {
-		assert.Equal(t, "35.201.180.4", cf.Hdfs.Host)
-		assert.NotEqual(t, int32(0), cf.Hdfs.Port)
-		assert.Equal(t, "root", cf.Hdfs.Username)
-	}
 }
 
 func TestReadDataConifg(t *testing.T) {
-	configPath := "../../config/testing.json"
+	configPath := "example.json"
 	cf := MustRead(configPath)
 	if cf.Data != nil {
 		assert.Equal(t, "./data/batches", cf.GetWorkspaceRootDir())
